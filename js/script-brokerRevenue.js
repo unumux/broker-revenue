18ff9c457e82e43f8c889760a46f5a8ef0e57f19
Dbay <gdbaileyii@gmail.com>
2014-12-12 14:41:03 -0500

    I added content, jquery and slider stuff to the index page

---
+++ js/script-brokerRevenue.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+	$('#slider-non-linear-step').noUiSlider({
+		start: [ 500, 4000 ],
+		range: {
+			'min': [     0 ],
+			'10%': [   500,  500 ],
+			'50%': [  4000, 1000 ],
+			'max': [ 10000 ]
+		}
+	});
+
+});
