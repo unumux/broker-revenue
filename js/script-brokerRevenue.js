2b421a5e68b8df9e6aaa33915e6e31b1d4647432
Tiffani <avx12@Voltron.local>
2015-03-06 15:19:50 -0500

    Changes for form accordion

--- js/script-brokerRevenue.js
+++ js/script-brokerRevenue.js
@@ -325,41 +325,49 @@ var
 
 $(document).ready(function () {
 
-	$("#sliderAccount").noUiSlider({
-		start: [ 20000 ],
-		step: 1000,
-		range: {
-			'min': [ 20000 ],
-			'max': [ 80000 ]
-		},
-		format: wNumb({
-			decimals: 3,
-			thousand: '.',
-			postfix: ' (US $)',
-		})
-	});
+	// $("#sliderAccount").noUiSlider({
+	// 	start: [ 20000 ],
+	// 	step: 1000,
+	// 	range: {
+	// 		'min': [ 20000 ],
+	// 		'max': [ 80000 ]
+	// 	},
+	// 	format: wNumb({
+	// 		decimals: 3,
+	// 		thousand: '.',
+	// 		postfix: ' (US $)',
+	// 	})
+	// });
 
-	$("#sliderAccount").noUiSlider({
-		start: [ 20 ],
-		step: 10,
-		range: {
-			'min': [ 20 ],
-			'max': [ 80 ]
-		},
-		format: {
-		  to: function ( value ) {
-			return value + ',-';
-		  },
-		  from: function ( value ) {
-			return value.replace(',-', '');
-		  }
+	// $("#sliderAccount").noUiSlider({
+	// 	start: [ 20 ],
+	// 	step: 10,
+	// 	range: {
+	// 		'min': [ 20 ],
+	// 		'max': [ 80 ]
+	// 	},
+	// 	format: {
+	// 	  to: function ( value ) {
+	// 		return value + ',-';
+	// 	  },
+	// 	  from: function ( value ) {
+	// 		return value.replace(',-', '');
+	// 	  }
+	// 	}
+	// });
+
+	// console.log($("#sliderAccount").val());
+
+	//$("#sliderAccount").Link("lower").to($("#numberOfAccounts"));
+
+	$('.element-toggle, .form-toggle').click(function(){
+		if($(this).next().hasClass('in')) {
+			$(this).next().collapse('toggle');
+		}
+		else {
+			$('.collapse').collapse('hide');
+			$(this).next().collapse('show');
 		}
 	});
-
-	console.log($("#sliderAccount").val());
-
-	$("#sliderAccount").Link("lower").to($("#numberOfAccounts"));
-
-	
 
 });
