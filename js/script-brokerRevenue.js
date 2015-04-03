19d709db3f6a184fdb434824f8bc404d9142df8a
Tyler Youngblood <tyleryoungblood@yahoo.com>
2015-04-03 15:03:35 -0400

    committing changes in master

--- js/script-brokerRevenue.js
+++ js/script-brokerRevenue.js
@@ -1,11 +1,48 @@
 $(document).ready(function () {
-	$(".panelToggle").click( function (e) {
-		e.preventDefault();
-		$(".toggled-content").each( function () {
-			$(this).removeClass("visible");
-		});
-		$(this).parent(".tile-heading").next(".tile-content").addClass("visible");
+	$(".element-toggle, .form-toggle").click(function () {
+		if($(this).next().hasClass("in")) {
+			$(this).next().collapse("toggle");
+		} else {
+			$(".collapse.in").collapse("hide");
+			$(this).next().collapse("show");
+		}
 	});
 
-});
 
+	// $("#sliderAccount").noUiSlider({
+	// 	start: [ 20000 ],
+	// 	step: 1000,
+	// 	range: {
+	// 		'min': [ 20000 ],
+	// 		'max': [ 80000 ]
+	// 	},
+	// 	format: wNumb({
+	// 		decimals: 3,
+	// 		thousand: '.',
+	// 		postfix: ' (US $)',
+	// 	})
+	// });
+
+	// $("#sliderAccount").noUiSlider({
+	// 	start: [ 20 ],
+	// 	step: 10,
+	// 	range: {
+	// 		'min': [ 20 ],
+	// 		'max': [ 80 ]
+	// 	},
+	// 	format: {
+	// 	  to: function ( value ) {
+	// 		return value + ',-';
+	// 	  },
+	// 	  from: function ( value ) {
+	// 		return value.replace(',-', '');
+	// 	  }
+	// 	}
+	// });
+
+	// console.log($("#sliderAccount").val());
+
+	//$("#sliderAccount").Link("lower").to($("#numberOfAccounts"));
+
+	
+});
